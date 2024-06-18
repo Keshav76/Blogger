d89454937869a39eb3b0adb239259fd84c6e6194
Keshav76 <keshav.banka2003@gmail.com>
2024-06-18 21:47:51 +0530

    bug fix

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Appbar from "../components/Appbar";
 import { useBlog } from "../hooks";
 import { useEffect } from "react";
